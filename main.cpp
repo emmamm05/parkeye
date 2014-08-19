832c2a19ed510c378f65509972fea502a06d09b6
Emmanuel Mora Marín <emmanuel@tupera.org>
2014-08-19 00:47:19 -0600

    comentarios

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "samples/canny_detector.h"
 #include "samples/smoothing.h"
 
+
+//pasar como argumento el path completo de la imagen lana.jpg
 int main( int argc, char** argv )
 {
+   //demo de varios algoritmos de blur para reducir ruido
    smoothing_demo(argc,argv);
+   //demo del algoritmo de canny detection para encontrar bordes
    canny_demo(argc,argv);
 }
