931d6f5c8ab8912132b7bda724ac960ca52dda0d
Emmanuel Mora Marín <emmanuel@tupera.org>
2014-08-19 00:10:46 -0600

    Estructura de folders

--- canny_detector.h
+++
@@ -1,7 +0,0 @@
-#ifndef CANNY_DETECTOR_H
-#define CANNY_DETECTOR_H
-
-static void CannyThreshold(int, void*);
-int canny_demo( int, char** argv );
-
-#endif // CANNY_DETECTOR_H
--- smoothing.h
+++
@@ -1,6 +0,0 @@
-#ifndef SMOOTHING_H
-#define SMOOTHING_H
-
- int smoothing_demo( int argc, char** argv );
-
-#endif // SMOOTHING_H
